9779e4d1e7a807cd39d17c2e9fa00cd8b003e421
Sophia <3160773@student.ocadu.ca>
2019-12-09 22:14:53 -0500

    update

--- Assignment-4/script.js
+++ Assignment-4/script.js
@@ -1,11 +1,22 @@
-$(document).ready(function(){
-    $(".button a").click(function(){
-        $(".overlay").fadeToggle(200);
-       $(this).toggleClass('btn-open').toggleClass('btn-close');
-    });
-});
-$('.overlay').on('click', function(){
-    $(".overlay").fadeToggle(200);   
-    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-    open = false;
+// $(document).ready(function(){
+//     $(".button a").click(function(){
+//         $(".overlay").fadeToggle(200);
+//        $(this).toggleClass('btn-open').toggleClass('btn-close');
+//     });
+// });
+// $('.overlay').on('click', function(){
+//     $(".overlay").fadeToggle(200);   
+//     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+//     open = false;
+// });
+
+
+$(".point").click(function(){
+
+
+  var target = $(this).attr("data-target");
+//  alert(target)
+
+  $(target).toggleClass("active")
+
 });
